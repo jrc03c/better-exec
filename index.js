88207bdacffacf24a929ce2ba590e55f01307aa7
jrc03c <jrc03c@pm.me>
2023-01-21 12:53:59 -0600

    ...

--- index.js
+++ index.js
@@ -1,38 +1,10 @@
-const { spawn } = require("child_process")
+const { execSync, spawn } = require("child_process")
+const fs = require("fs")
+const process = require("process")
 
-function betterExec(command, shouldBeSilent, callback) {
-  function compileResults() {
-    if (stdoutTemp.length > 0) {
-      const finalLines = stdoutTemp.split("\n")
-
-      finalLines
-        .slice(0, finalLines.at(-1).trim().length === 0 ? -1 : null)
-        .forEach(line => {
-          stdoutLines.push(line)
-
-          if (!shouldBeSilent) {
-            console.log(line)
-          }
-        })
-    }
-
-    if (stderrTemp.length > 0) {
-      const finalLines = stderrTemp.split("\n")
-
-      finalLines
-        .slice(0, finalLines.at(-1).trim().length === 0 ? -1 : null)
-        .forEach(line => {
-          stderrLines.push(line)
-
-          if (!shouldBeSilent) {
-            console.log(line)
-          }
-        })
-    }
-
-    const stdout = stdoutLines.join("\n")
-    const stderr = stderrLines.join("\n")
-    return { stdout, stderr }
+function betterExec(commands, shouldBeSilent, callback) {
+  if (typeof commands === "string") {
+    commands = [commands]
   }
 
   if (typeof shouldBeSilent === "function") {
@@ -40,64 +12,90 @@ function betterExec(command, shouldBeSilent, callback) {
     shouldBeSilent = false
   }
 
-  const stdoutLines = []
-  const stderrLines = []
-  let stdoutTemp = ""
-  let stderrTemp = ""
+  let stdout = ""
+  let stderr = ""
+  let cwd = process.cwd()
+  const origCwd = cwd
 
   return new Promise((resolve, reject) => {
     try {
-      const child = spawn(command.split(" ")[0], command.split(" ").slice(1))
+      async function next() {
+        index++
 
-      child.stdout.setEncoding("utf8")
-      child.stderr.setEncoding("utf8")
-
-      child.stdout.on("data", data => {
-        data = data.toString()
-        stdoutTemp += data
-
-        if (stdoutTemp.includes("\n")) {
-          const parts = stdoutTemp.split("\n")
-          const newline = parts[0]
-          const remainder = parts.slice(1).join("\n")
-          stdoutLines.push(newline)
-          stdoutTemp = remainder
-
-          if (!shouldBeSilent) {
-            console.log(newline)
-          }
-        }
-      })
-
-      child.stderr.on("data", data => {
-        data = data.toString()
-        stderrTemp += data
-
-        if (stderrTemp.includes("\n")) {
-          const parts = stderrTemp.split("\n")
-          const newline = parts[0]
-          const remainder = parts.slice(1).join("\n")
-          stderrLines.push(newline)
-          stderrTemp = remainder
-
-          if (!shouldBeSilent) {
-            console.log(newline)
-          }
-        }
-      })
-
-      child.on("close", code => {
-        const results = compileResults()
-
-        if (code > 0) {
-          return reject(results)
-        } else {
+        if (index >= commands.length) {
+          const results = { stdout, stderr }
           if (callback) callback(results)
           return resolve(results)
         }
-      })
+
+        const command = commands[index]
+
+        if (typeof command === "function") {
+          const out = command()
+
+          if (out instanceof Promise) {
+            return out.then(() => next())
+          } else {
+            return next()
+          }
+        }
+
+        if (command.match(/^\bcd\b/g)) {
+          cwd = command.split(" ")[1]
+
+          if (!fs.existsSync(cwd)) {
+            try {
+              cwd = execSync(cwd, { encoding: "utf8" })
+            } catch (e) {
+              throw new Error(`"${cwd}" doesn't seem to be a valid directory!`)
+            }
+          }
+
+          return next()
+        }
+
+        try {
+          process.chdir(cwd)
+        } catch (e) {
+          process.chdir(origCwd)
+        }
+
+        const child = spawn(command.split(" ")[0], command.split(" ").slice(1))
+
+        child.stdout.setEncoding("utf8")
+        child.stderr.setEncoding("utf8")
+
+        child.stdout.on("data", data => {
+          data = data.toString()
+          stdout += data
+
+          if (!shouldBeSilent) {
+            process.stdout.write(data)
+          }
+        })
+
+        child.stderr.on("data", data => {
+          data = data.toString()
+          stderr += data
+
+          if (!shouldBeSilent) {
+            process.stdout.write(data)
+          }
+        })
+
+        child.on("close", code => {
+          if (code > 0) {
+            return reject(results)
+          } else {
+            return next()
+          }
+        })
+      }
+
+      let index = -1
+      return next()
     } catch (e) {
-      const results = compileResults()
+      const results = { stdout, stderr }
       results.error = e
       return reject(results)
     }
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Intro
 
-`better-exec` is just a version of [`child_process.exec`](https://nodejs.org/api/child_process.html#child_processexeccommand-options-callback) that prints output in realtime rather than all at once at the end of the execution.
+`better-exec` is just a version of [`child_process.exec`](https://nodejs.org/api/child_process.html#child_processexeccommand-options-callback) that prints output in (almost) realtime rather than all at once at the end of the execution. It also offers the option to run multiple commands, optionally interspersed with JS function calls.
 
 # Installation
 
@@ -28,6 +28,34 @@ exec("wget https://example.com/path/to/some-file").then(results => {
   // do something with stdout & stderr
   console.log("Done!")
 })
+
+// run multiple commands
+const commands = [
+  "cd /my/favorite/directory",
+  "npm install --save my-favorite-package",
+]
+
+exec(commands).then(() => {
+  console.log("Done!")
+})
+
+// mix commands and function calls
+const fs = require("fs")
+const path = require("path")
+const dir = "/my/favorite/directory"
+
+const commands = [
+  `cd "${dir}"`,
+  () =>
+    fs.writeFileSync(
+      path.join(dir, "comments.txt"),
+      "Here are my comments!",
+      "utf8"
+    ),
+  "cat comments.txt",
+]
+
+exec(commands).then(...)
 ```
 
 In the above examples, you can do something with `stdout` and `stderr` (from the `results` object) if you want, but do note that they'll already have been printed to the command line by the time you see them in the callback function.
@@ -44,4 +72,43 @@ exec("wget https://example.com/path/to/some-file", silent, results => {
   const { stdout, stderr, error } = results
   // ...
 })
+```
+
+# Caveats
+
+## Running multiple commands
+
+One major caveat is that you shouldn't try to run multiple commands in single string. For example, this is a bad idea:
+
+```js
+exec("cd /my/favorite/directory && npm install --save my-favorite-package")
+```
+
+Instead, it should be broken into multiple commands, like this:
+
+```js
+const commands = [
+  "cd /my/favorite/directory",
+  "npm install --save my-favorite-package",
+]
+
+exec(commands).then(...)
+```
+
+## Changing directories
+
+There's only one command that isn't literally run as given: `cd`. Instead of literally spawning a process that'll run the `cd` command, the current process's working directory is changed via `process.chdir("/some/path")`. I don't think this'll cause many problems so long as you're doing pretty basic stuff. But if you try to get fancy — like, for example, by changing to a directory value that's computed at runtime in a nested command — then I don't know what'll happen. If you need to compute something at runtime, maybe interject a JS function call to compute the value, and then use that value in the next command. For example:
+
+```js
+const root = "/my/dir"
+let id
+
+const commands = [
+  `cd "${root}"`,
+  () => (id = Math.random().toString().split(".")[1]),
+  `mkdir -p "${id}"`,
+  // etc.
+]
+
+exec(commands)
 ```
